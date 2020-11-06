18c3a8f4eb6760fc1f5b7169ce435dd67b20878c
dbacy <69469697+dbacy@users.noreply.github.com>
2020-11-06 03:26:25 -0600

    update

--- Day1R.R
+++ Day1R.R
@@ -1,2 +1,65 @@
+8^2
+ 
+x<- 5
+
+x <- 5 
+
+x <- 8
+
+x
+
+ 
+objects()
+
+search()
+  
+x <- c(1:10)
+
+y <- c(11:20)
+
+plot(x,y)
+
+x<- 8
+
+typeof(x)
+
+is.integer(x)
+
+is.double(x)
+
+x <- as.integer(x)
+
+typeof(x)
+
+is.integer(x)
+
+is.double(x)
+
+y <- 8 + 14i
+
+typeof(y)
+
+is.double(y)
+
+y <- as.double(y)
+
+is.double(y)
+
+w <- 7 < 8
+
+w
+
+w <- as.integer(w)
+
+w
+
+typeof(w)
+
+w <- as.integer(7<8)
+
+w
+
+typeof(w)
+
 
 
